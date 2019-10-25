6147df20444d0351650da49b7501f30f1b41841c
Ryan David Forsyth <forsyth.r@gmail.com>
2019-10-24 23:15:23 -0400

    Initial commit

--- Splash/AppDelegate.swift
+++ Splash/AppDelegate.swift
@@ -2,8 +2,6 @@
 //  AppDelegate.swift
 //  Splash
 //
-//  Created by Gwinyai on 2/7/2018.
-//  Copyright © 2018 Gwinyai. All rights reserved.
 //
 
 import UIKit
@@ -14,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
--- Splash/ViewController.swift
+++ Splash/ViewController.swift
@@ -2,24 +2,67 @@
 //  ViewController.swift
 //  Splash
 //
-//  Created by Gwinyai on 2/7/2018.
-//  Copyright © 2018 Gwinyai. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let twitterImage = UIImageView(image: UIImage(named: "twitter")!)
+    
+    let splashView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        splashView.backgroundColor = UIColor(red: 0/255, green: 172/255, blue: 237/255, alpha: 1.0)
+        
+        view.addSubview(splashView)
+        
+        splashView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        
+        twitterImage.contentMode = .scaleAspectFit
+        
+        splashView.addSubview(twitterImage)
+        
+        twitterImage.frame = CGRect(x: splashView.frame.midX - 37, y: splashView.frame.midY - 35, width: 70, height: 70)
+        
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.scaleDownUpAnimation()
+        }
+    }
+    
+    
+    func scaleDownUpAnimation() {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.twitterImage.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
 
+        }) { (success) in
+            
+            UIView.animate(withDuration: 0.35, delay: 0.1, options: .curveEaseIn, animations: {
+                
+                self.twitterImage.transform = CGAffineTransform(scaleX: 5, y: 5)
+                
+            }, completion: { (success) in
+                
+                self.removeSplashScreen()
+                
+            })
+            
+        }
+    }
+    
+    func removeSplashScreen() {
+        
+        splashView.removeFromSuperview()
+    }
 
 }
 
